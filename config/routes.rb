2b969c1327b6fba25d3c4049bec56fd7e96e7fb6
Hiroyuki Nakajima <hiroyuki.nakajima@theport.jp>
2019-06-25 12:15:40 +0900

    Add Instant Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'top#index'
   get '/signup', to:'users#new'
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   get '/logout', to:'sessions#destroy'
 
+  resources :users
   resources :articles
 end
