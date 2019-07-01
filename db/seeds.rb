8642e95e6a6d201a8dda6691d9c9d2b1dd7fb7c8
Hiroyuki Nakajima <hiroyuki.nakajima@theport.jp>
2019-07-01 17:09:41 +0900

    マイグレーションファイルのファイル名変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create( name: "example_user", email: "test@foo.bar", password: "testtest" )
+user.save
+
+article = Article.create( content: "hello,world", user_id: 1, title:"greeting")
+article.save
