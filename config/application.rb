b077f771720fb6ef33e869421aee2f1b2da2eab9
Hiroyuki Nakajima <hiroyuki.nakajima@theport.jp>
2019-07-02 12:08:58 +0900

    記事単体のレイアウトを修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module BlogApp
     config.load_defaults 5.1
 
     config.i18n.default_locale = :ja
+    #config.i18n.fallbacks = [:ja]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
